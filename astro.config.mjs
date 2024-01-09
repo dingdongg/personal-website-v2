3f40a547356061677862a0e5b517194c149cc890
dingdongg <cs.dg.kim@gmail.com>
2024-01-09 10:39:05 -0800

    try prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  prefetch: {
+    defaultStrategy: "load",
+  },
 });
