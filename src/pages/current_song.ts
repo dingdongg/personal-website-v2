a6aa64694c18d83cf0eb83410f3b0274791bf157
dingdongg <cs.dg.kim@gmail.com>
2024-01-13 18:23:14 -0800

    handle failure case

--- src/pages/current_song.ts
+++ src/pages/current_song.ts
@@ -29,7 +29,13 @@ export async function GET() {
 		});
 
 		if (response.status === 204 || response.status > 400) {
-			return { isPlaying : false };
+			return new Response(
+				JSON.stringify({ isPlaying: false }), {
+					headers: {
+						"Cache-Control": "public, s-maxage=60, stale-while-revalidate=30",
+					},
+				},
+			);
 		}
 
 		const parsedRes = await response.json();
