f96f1b8a108853cc95c48fcf464c231e6590a27c
dingdongg <cs.dg.kim@gmail.com>
2024-01-13 19:22:17 -0800

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "server",
   prefetch: {
-    defaultStrategy: "load",
+    defaultStrategy: "load"
   },
+  adapter: netlify()
 });
