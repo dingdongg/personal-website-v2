b3ce2dd27da829fc9b8122e1391b959a7af5cde5
dingdongg <cs.dg.kim@gmail.com>
2024-05-23 20:46:21 -0600

    fix responsive design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				"pc": "1024px",
+				"mobile": "400px",
+			},
+		},
 	},
 	plugins: [],
 }
