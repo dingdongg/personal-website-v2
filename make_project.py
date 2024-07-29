a5a59436e3bf7c2cb723c706adb538d9c2490b27
dingdongg <cs.dg.kim@gmail.com>
2024-07-28 21:20:39 -0600

    complete project generation script

--- make_project.py
+++ make_project.py
@@ -1,3 +1,4 @@
+import json
 from typing import Optional
 from datetime import datetime
 
@@ -33,6 +34,10 @@ def make_project():
     repoUrl = input("> Source code URL? (<enter to skip>, else paste link + enter) ")
     url = repoUrl if repoUrl != "" else None
 
+    pubDate = input("> Creation date? (<enter to skip>, YYYY-MM + enter) ")
+
+    description = input("> Brief description of project (enter to submit): \n")
+
     while not techStackCorrect:
         techStack = input("> Tech stack? (separate with spaces): ")
         items = [f"- {t}\n" for t in techStack.strip().split(" ")]
@@ -48,15 +53,35 @@ def make_project():
 
     confirmation = input("> Confirm? (y/n) ") == "y"
 
+    args = [title, techStack, url]
+    args.append(datetime.strptime(pubDate, "%Y-%m") if pubDate != "" else datetime.today())
+
     if confirmation:
-        filename: str = title.strip().lower().replace(" ", "-") + ".md"
-        f = open(f"./src/pages/projects/{filename}", "a")
-        f.write(init_boilerplate(title, techStack, repoUrl=url))
+        filename: str = title.strip().lower().replace(" ", "-")
+        f = open(f"./src/pages/projects/{filename}.md", "a")
+        f.write(init_boilerplate(*args))
         f.close()
         print(f"> File created at /src/pages/projects/{filename}")
+        month, year = args[-1].month, args[-1].year
+        update_json(title, description, f"/projects/{filename}", month, year)
     else:
         print("project creation cancelled.")
 
+def update_json(title: str, desc: str, url: Optional[str], pubMonth: int, pubYear: int):
+    f = open("test.json", "r+")
+    data = json.load(f)
+    data.append({
+        "title": title,
+        "url": url,
+        "pubDate": { "month": pubMonth, "year": pubYear },
+        "description": desc,
+    })
+
+    data.sort(reverse=True, key=lambda i: i["pubDate"]["year"] + i["pubDate"]["month"] / 100)
+    f.seek(0)
+    json.dump(data, f, indent=4)
+    f.truncate()
+    f.close()
 
 if __name__ == "__main__":
     make_project()
